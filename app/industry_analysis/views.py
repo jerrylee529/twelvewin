0d3a3904054c83427cfe4788580b5656ba8735a6
jerrylee529 <lwzwj@163.com>
2019-04-04 16:22:35 +0800

    add the function that get industry information about one stock

--- app/industry_analysis/views.py
+++ app/industry_analysis/views.py
@@ -192,3 +192,100 @@ def update_labels():
     return jsonify(myClassJson)
 
 
+@industry_analysis_blueprint.route('/industry/stock/<code>', methods=['GET'])
+def get_industry_stock(code):
+    industry = request.values.get('industry', '')
+    cluster_type = int(request.values.get('cluster_type', 0))
+
+    log.info("code: {}, industry: {}, cluster type: {}, {}".format(code, industry, cluster_type, type(cluster_type)))
+
+    if industry is None or industry == '':
+        return jsonify({"total": 0, "rows": []})
+
+    if cluster_type == 0:
+        instruments = analyzer.industry_cluster_basic(industry)
+    else:
+        instruments = analyzer.industry_cluster_by_tech(industry, '2018-01-01')
+
+    if instruments is None or len(instruments) <= 0:
+        return jsonify({"total": 0, "rows": []})
+
+    id = 1
+    rows = []
+    total_pe = 0
+    total_pb = 0
+    total_esp = 0
+    total_bvps = 0
+    total_gpr = 0
+    total_npr = 0
+    pe_list = []
+    pb_list = []
+    esp_list = []
+    bvps_list = []
+    gpr_list = []
+    npr_list = []
+
+    label = instruments['label'][code]
+
+    for index, instrument in instruments.iterrows():
+        total_pe += instrument.pe
+        total_pb += instrument.pb
+        total_esp += instrument.esp
+        total_bvps += instrument.bvps
+        total_gpr += instrument.gpr
+        total_npr += instrument.npr
+        pe_list.append(instrument.pe)
+        pb_list.append(instrument.pb)
+        esp_list.append(instrument.esp)
+        bvps_list.append(instrument.bvps)
+        gpr_list.append(instrument.gpr)
+        npr_list.append(instrument.npr)
+
+        if instrument['label'] != label:
+            continue
+
+        item = {}
+        item['id'] = id
+        #item['label'] = clusters.loc[index, 'label'] + 1
+        item['label'] = instrument['label'] + 1
+        item['industry'] = instrument['industry']
+        item['code'] = index
+        item['name'] = instrument['name']
+        item['pe'] = instrument['pe']
+        #item['outstanding'] = instrument['outstanding']
+        #item['totals'] = instrument['totals']
+        #item['total_assets'] = instrument['totalAssets']
+        #item['liquid_assets'] = instrument['liquidAssets']
+        #item['fixed_assets'] = instrument['fixedAssets']
+        #item['esp'] = round(instrument['esp'], 2)
+        #item['bvps'] = instrument['bvps']
+        item['pb'] = instrument['pb']
+        #item['time_2_market'] = instrument['timeToMarket']
+        item['gpr'] = instrument['gpr']
+        item['npr'] = instrument['npr']
+        #item['holders'] = instrument['holders']
+        rows.append(item)
+        id += 1
+
+    rows = sorted(rows, key=lambda e: e.__getitem__('label'))
+
+    result_data = {}
+    rows_len = len(rows)
+    result_data['total'] = rows_len
+    result_data['rows'] = rows
+
+    if rows_len > 0:
+        result_data['avg_pe'] = round(total_pe/rows_len, 2)
+        result_data['avg_pb'] = round(total_pb/rows_len, 2)
+        result_data['avg_esp'] = round(total_esp/rows_len, 2)
+        result_data['avg_bvps'] = round(total_bvps/rows_len, 2)
+        result_data['avg_gpr'] = round(total_gpr/rows_len, 2)
+        result_data['avg_npr'] = round(total_npr/rows_len, 2)
+        result_data['mid_pe'] = round(get_median(pe_list), 2)
+        result_data['mid_pb'] = round(get_median(pb_list), 2)
+        result_data['mid_esp'] = round(get_median(esp_list), 2)
+        result_data['mid_bvps'] = round(get_median(bvps_list), 2)
+        result_data['mid_gpr'] = round(get_median(gpr_list), 2)
+        result_data['mid_npr'] = round(get_median(npr_list), 2)
+
+    return jsonify(result_data)
